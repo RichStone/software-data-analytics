b88e5828d402bd0ee64f50c4a06ea83d1ba6889b
RichStone <richard.had@hotmail.de>
2019-05-10 12:53:39 +0200

    fix encoding issue

--- submission/1_2_line_count/line_count.py
+++ submission/1_2_line_count/line_count.py
@@ -2,15 +2,12 @@ import os
 
 
 def count_lines(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding='utf8', errors='replace') as f:
         return len(f.readlines())
 
 
+# traverse dir
 for root, dirs, files in os.walk('.'):
     for filename in files:
-        try:
-            LOC = count_lines(os.path.join(root, filename))
-            print('{0};{1}'.format(filename, LOC))
-        except UnicodeDecodeError:
-            # print('Could not count lines of file <<{0}>>'.format(filename))
-            pass
+        path = os.path.join(root, filename)
+        print('{0};{1}'.format(filename, count_lines(path)))
